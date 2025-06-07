6b2cd0c1ac81920d0c182a418826cf02e66c6b7a
rach-md <rach-md@users.noreply.github.com>
2025-06-07 13:59:45 +0800

    workflows, build.sh: only build 64bit

--- build.sh
+++ build.sh
@@ -6,61 +6,28 @@ main() {
     clang_root=$(pwd)/clang_root
     buildroot=$(pwd)
     srcdir=$(pwd)/src_packages
-    local target=$1
-    compiler=$2
-    simple_package=$3
+    bit="64"
+    arch="x86_64"
+    compiler=$1
+    simple_package=$2
 
     prepare
-    if [ "$target" == "32" ]; then
-        package "32" 
-    elif [ "$target" == "64" ]; then
-        package "64"
-    elif [ "$target" == "64-v3" ]; then
-        package "64-v3"
-    elif [ "$target" == "aarch64" ]; then
-        package "aarch64"
-    elif [ "$target" == "all-64" ]; then
-        package "64"
-        package "64-v3"
-        package "aarch64"
-    else [ "$target" == "all" ];
-        package "32"
-        package "64"
-        package "64-v3"
-        package "aarch64"
-    fi
+    package
     rm -rf ./release/mpv-packaging-master
 }
 
 package() {
-    local bit=$1
-    if [ $bit == "32" ]; then
-        local arch="i686"
-    elif [ $bit == "64" ]; then
-        local arch="x86_64"
-    elif [ $bit == "64-v3" ]; then
-        local arch="x86_64"
-        local gcc_arch="-DGCC_ARCH=x86-64-v3"
-        local x86_64_level="-v3"
-    elif [ $bit == "aarch64" ]; then
-        local arch="aarch64"
-    fi
-
-    build $bit $arch $gcc_arch
-    zip $bit $arch $x86_64_level
+    build $bit $arch
+    zip $bit $arch
     sudo rm -rf $buildroot/build$bit/mpv-*
     sudo chmod -R a+rwx $buildroot/build$bit
 }
 
 build() {
-    local bit=$1
-    local arch=$2
-    local gcc_arch=$3
-    
     if [ "$compiler" == "clang" ]; then
         clang_option=(-DCMAKE_INSTALL_PREFIX=$clang_root -DMINGW_INSTALL_PREFIX=$buildroot/build$bit/install/$arch-w64-mingw32 -DCLANG_PACKAGES_LTO=ON)
     fi
-    cmake -Wno-dev --fresh -DTARGET_ARCH=$arch-w64-mingw32 $gcc_arch -DCOMPILER_TOOLCHAIN=$compiler "${clang_option[@]}" $extra_option -DENABLE_CCACHE=ON -DSINGLE_SOURCE_LOCATION=$srcdir -DRUSTUP_LOCATION=$buildroot/install_rustup -G Ninja -H$gitdir -B$buildroot/build$bit
+    cmake -Wno-dev --fresh -DTARGET_ARCH=$arch-w64-mingw32 -DCOMPILER_TOOLCHAIN=$compiler "${clang_option[@]}" $extra_option -DENABLE_CCACHE=ON -DSINGLE_SOURCE_LOCATION=$srcdir -DRUSTUP_LOCATION=$buildroot/install_rustup -G Ninja -H$gitdir -B$buildroot/build$bit
 
     ninja -C $buildroot/build$bit download || true
 
@@ -90,17 +57,13 @@ build() {
 }
 
 zip() {
-    local bit=$1
-    local arch=$2
-    local x86_64_level=$3
-
     mv $buildroot/build$bit/mpv-* $gitdir/release
     if [ "$simple_package" != "true" ]; then
         cd $gitdir/release/mpv-packaging-master
-        cp -r ./mpv-root/* ../mpv-$arch$x86_64_level*
+        cp -r ./mpv-root/* ../mpv-$arch*
     fi
     cd $gitdir/release
-    for dir in ./mpv*$arch$x86_64_level*; do
+    for dir in ./mpv*$arch*; do
         if [ -d $dir ]; then
             7z a -m0=lzma2 -mx=9 -ms=on $dir.7z $dir/* -x!*.7z
             rm -rf $dir
@@ -134,14 +97,13 @@ prepare() {
     fi
 }
 
-while getopts t:c:s:e: flag
+while getopts c:s:e: flag
 do
     case "${flag}" in
-        t) target=${OPTARG};;
         c) compiler=${OPTARG};;
         s) simple_package=${OPTARG};;
         e) extra_option=${OPTARG};;
     esac
 done
 
-main "${target:-all-64}" "${compiler:-gcc}" "${simple_package:-false}"
+main "${compiler:-gcc}" "${simple_package:-false}"

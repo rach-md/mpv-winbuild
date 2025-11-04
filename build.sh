76365805cee43018c404b2d78b521bf477ff82aa
rach-md <rach-md@users.noreply.github.com>
2025-11-04 20:12:20 +0800

    workflows/mpv, build: remove lgpl option & patch

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ zip() {
     cd $gitdir/release
     for dir in ./mpv*$arch*; do
         if [ -d $dir ]; then
-            7z a -m0=lzma2 -mx=9 -ms=on $dir.7z $dir/* -x!*.7z
+            7z a -m0=lzma2 -mx=9 -ms=on mpv.7z $dir/* -x!*.7z
             rm -rf $dir
         fi
     done

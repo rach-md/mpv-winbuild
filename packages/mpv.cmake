000a8e70e660df3bada81d2767fbb0545d2d0310
rach-md <rach-md@users.noreply.github.com>
2025-12-10 15:45:20 +0800

    mpv,workflows/mpv: simplify

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -36,29 +36,12 @@ ExternalProject_Add(mpv
 
 ExternalProject_Add_Step(mpv copy-binary
     DEPENDEES build
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.exe                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.com                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.com
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.pdf                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/doc/manual.pdf
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.exe ${CMAKE_BINARY_DIR}/mpv/mpv.exe
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.com ${CMAKE_BINARY_DIR}/mpv/mpv.com
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.pdf ${CMAKE_BINARY_DIR}/mpv/doc/manual.pdf
     COMMENT "Copying mpv binaries and manual"
-)
-
-set(RENAME ${CMAKE_CURRENT_BINARY_DIR}/mpv-prefix/src/rename.sh)
-file(WRITE ${RENAME}
-"#!/bin/bash
-cd $1
-GIT=$(git rev-parse --short=7 HEAD)
-mv $2 $2-git-\${GIT}")
-
-ExternalProject_Add_Step(mpv copy-package-dir
-    DEPENDEES copy-binary
-    COMMAND chmod 755 ${RENAME}
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-package ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-
-    COMMENT "Moving mpv package folder"
-    LOG 1
 )
 
 force_rebuild_git(mpv)
 force_meson_configure(mpv)
-cleanup(mpv copy-package-dir)
+cleanup(mpv copy-binary)
